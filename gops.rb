c4c906e260bc3395d592dc5149f9f40b9130154e
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-01 15:31:56 -0400

    gops version v0.0.7

--- gops.rb
+++ gops.rb
@@ -1,9 +1,9 @@
 class Gops < Formula
   desc "GOlang Project Sample."
   homepage "https://github.com/tunedmystic/gops"
-  url "https://github.com/tunedmystic/gops/releases/download/v0.0.6/gops_0.0.6_macOS_64-bit.tar.gz"
-  version "0.0.6"
-  sha256 "c18c7f231ebffd5b488f603e053265d211a49c9c9081b6fea6e49ecc45caa842"
+  url "https://github.com/tunedmystic/gops/releases/download/v0.0.7/gops_0.0.7_macOS_64-bit.tar.gz"
+  version "0.0.7"
+  sha256 "9019e435209b2f22341773136c3fca9d47bb8d8bdebc638e3adbce2738c55c48"
 
   def install
     bin.install "gops"
